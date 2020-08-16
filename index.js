40bb1a648489b11a7ad8581b88e7aac60dbb900c
Cheng Wang <cwaf17@gmail.com>
2020-08-16 18:34:36 -0400

    Fixed index.js typo

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use(passport.session());
 require("./routes/authRoutes")(app); // goes to the module.exports function in the authRoutes file with the parameter app
 require("./routes/billingRoutes")(app); //valid because the require statement returns a function which we immediately call with the parameter (app
 
-if (process.send.NODE_ENV == "production") {
+if (process.env.NODE_ENV == "production") {
   // Express will serve up production assets
   // like our main.js file or main.css file
   app.use(express.static("client/build"));
